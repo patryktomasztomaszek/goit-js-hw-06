c1fc7f106f928a13523ad5756eee327e082169e2
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-05-19 21:11:59 +0200

    Task02
    Succesfully implemented task02 js code

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const task02 = () => {
+  let ingredientList = ""
+  const list = document.querySelector("#ingredients");
+  ingredients.forEach(ingredient => {
+     const li = document.createElement("li");
+     li.className = "item"
+     li.textContent = ingredient;
+     list.append(li);
+  });
+}
+task02();
